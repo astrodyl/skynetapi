a4d9d56f60351326bdff6db32a6694e78fba56b8
Dylan <astrodyl@live.unc.edu>
2024-11-15 12:15:50 -0500

    Allow DownloadRequest to be Importable

--- skynetapi/__init__.py
+++ skynetapi/__init__.py
@@ -1,5 +1,6 @@
 from .requests.obs_request import ObservationRequest
+from .requests.dl_request import DownloadRequest
 from .requests.exps_request import ExposureRequest
 from .requests.http_request import HTTPRequest
 
-__all__ = ['ObservationRequest', 'ExposureRequest', 'HTTPRequest']
+__all__ = ['ObservationRequest', 'DownloadRequest', 'ExposureRequest', 'HTTPRequest']
